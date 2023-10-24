ae10225254a28750eb5e2f55fa6dbb609b1a2689
Justin <jlke45@gmail.com>
2023-10-24 12:42:03 +0900

    Revise logic for creating the grid.
    Fixed issue with the row class not being added to the div when created
    in the for loop. Also added an inner for loop to create cells within
    the rows.

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const container = document.querySelector(".container");
 
 for (let i = 0; i < 16; i++) {
     let row = document.createElement("div");
-    console.log("Row made");
-    row.classList.add(".row");
+    row.classList.add("row");
     container.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        row.appendChild(cell);
+    }
 }
