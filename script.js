22027d7e1dbb3a862ddd3522620350d40b6f674f
Justin <jlke45@gmail.com>
2023-10-24 19:35:47 +0900

    Add logic for changing cell color on mouseover

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const container = document.querySelector(".container");
+let cellNumber = 16;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < cellNumber; i++) {
     let row = document.createElement("div");
     row.classList.add("row");
     container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < cellNumber; j++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
         row.appendChild(cell);
     }
 }
+
+cellList = document.querySelectorAll(".cell");
+
+cellList.forEach(cell => {
+    cell.addEventListener("mouseover", () => {
+        cell.style.backgroundColor = "black";
+    })
+})
