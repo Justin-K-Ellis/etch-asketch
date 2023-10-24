18626cbdc34d7c45c8aaadb7faf4dce8dd4270a8
Justin <jlke45@gmail.com>
2023-10-24 23:45:56 +0900

    Reorganize code

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+const outerContainer = document.querySelector(".outer-container");
 const container = document.querySelector(".container");
+const buttons = document.querySelector(".buttons");
+const resetBtn = document.querySelector("#reset");
+const cellChangeBtn = document.querySelector("#cell-change");
+
+let displayCellNum = document.querySelector("#cell-num");
 let cellNumber = 16;
 
-for (let i = 0; i < cellNumber; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for (let j = 0; j < cellNumber; j++) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        row.appendChild(cell);
-    }
-}
+
+drawGrid(cellNumber);
 
 cellList = document.querySelectorAll(".cell");
 
@@ -19,3 +17,33 @@ cellList.forEach(cell => {
         cell.style.backgroundColor = "black";
     })
 })
+
+displayCellNum.textContent = `Size: ${cellNumber} x ${cellNumber} (${cellNumber * cellNumber} cells)`
+outerContainer.insertBefore(displayCellNum, buttons);
+
+resetBtn.addEventListener("click", () => {
+    location.reload();
+})
+
+
+cellChangeBtn.addEventListener("click", () => {
+    let n = prompt("Enter a number:")
+    n = Number(n);
+    console.log(typeof n);
+    // TODO: Remove all child nodes from container div before redrawing the grid.
+    drawGrid(n);
+})
+
+
+function drawGrid(n) {
+    for (let i = 0; i < n; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for (let j = 0; j < n; j++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
+        }
+    }
+}
